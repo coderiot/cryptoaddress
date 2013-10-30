5995c91dcddeb0fcf114c87cb05050c892f1bf48
peterrr <peterrr@github>
2013-10-30 10:22:08 +0100

    initial package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='cryptoaddress',
+      version='0.1.0',
+      license='GPL',
+      description='',
+      author='peterr',
+      author_email='coderiot@zoho.com',
+      url='https://github.com/coderiot/cryptoaddress/',
+      packages=['cryptoaddress'],
+)
