9f82299f1feb44bdd746ff6c32774ce11f631622
peterrr <peterrr@github>
2013-11-01 11:52:18 +0100

    add description for package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(name='cryptoaddress',
       version='0.1.0',
       license='GPL',
-      description='',
+      description='validate, convert and detect addresses from different crypto currencies.',
       author='peterr',
       author_email='coderiot@zoho.com',
       url='https://github.com/coderiot/cryptoaddress/',
